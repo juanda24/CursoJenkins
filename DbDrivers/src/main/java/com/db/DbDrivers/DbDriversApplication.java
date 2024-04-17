87f22cb6fc761b971eba412e895c40d805a7e14c
juanda24 <juangastrife@gmail.com>
2024-04-17 10:26:46 +0200

    Add files via upload
    Maven project with db drivers for Jenkins Maven job

---
+++ DbDrivers/src/main/java/com/db/DbDrivers/DbDriversApplication.java
@@ -0,0 +1,13 @@
+package com.db.DbDrivers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDriversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbDriversApplication.class, args);
+	}
+
+}
---
+++ DbDrivers/src/test/java/com/db/DbDrivers/DbDriversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.DbDrivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDriversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
